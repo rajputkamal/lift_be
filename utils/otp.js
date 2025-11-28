ce5e0c06e5609b1979cecff9837a93857b692304
rajputkamal <rajput.kamal40@gmail.com>
2025-11-28 16:18:36 +0530

    Removed twilio dependency and otp functionality

--- utils/otp.js
+++ utils/otp.js
@@ -1,28 +1,6 @@
 import dotenv from "dotenv";
-import Twilio from "twilio";
 
 dotenv.config();
-
-const client = new Twilio(
-  process.env.TWILIO_ACCOUNT_SID,
-  process.env.TWILIO_AUTH_TOKEN
-);
-
-//TODO:: Remove Twilio config once 2FACTOR setup is done.
-export const sendOtpOnUserNumber = async (phoneNumber, otp) => {
-  try {
-    await client.messages.create({
-      body: `Your verification code for LIFT RIDE sharing app is ${otp}. Please do not share with anyone. It is valid for 3 minutes.`,
-      from: process.env.TWILIO_PHONE_NUMBER,
-      to: `+91${phoneNumber}`,
-    });
-  } catch (error) {
-    console.error(
-      "We are facing an error while sending OTP. Please try again.",
-      error
-    );
-  }
-};
 
 export const generateOtpCode = () =>
   Math.floor(1000 + Math.random() * 9000).toString();
